1d4bda04299118fed7941071ac686b042931029d
lasker61 <laskernahid2@gmail.com>
2021-02-28 21:25:26 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
 
 function App() {
+const [countries,setCountries]=useState([]);
+
+useEffect(() =>{
+  fetch('https://restcountries.eu/rest/v2/all')
+  .then(response =>response.json())
+  .then(data =>{setCountries(data);
+    const names = data.map(country => country.name);
+    console.log(names);
+  })
+  .catch(error=>console.log(error))
+
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>country loaded:{countries.length}</h1>
+      <ul>
+        {
+          countries.map(country=><Country name={country.name}></Country>)
+        }
+
+      </ul>
+      
+     
     </div>
   );
 }
---
+++ src/components/Country/Country.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = (props) => {
+    return (
+        <div>
+            <h5>{props.name}</h5>
+        </div>
+    );
+};
+
+export default Country;
